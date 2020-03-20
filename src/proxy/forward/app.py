6b181bb81bce43bf753de7bcd6472d7a6f369a3f
Javier Munoz <javimudi@users.noreply.github.com>
2020-03-20 17:43:28 +0100

    wip(proxy): proper forward

--- src/proxy/forward/app.py
+++ src/proxy/forward/app.py
@@ -6,30 +6,14 @@ import boto3
 
 sqs = boto3.client('sqs')
 queue_url = os.getenv('SQS_URL')
+bucket_name = os.getenv('BUCKET_NAME')
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
 
     dedup = str(uuid4())
+
+    body = event.get('body')
+
 
     sqs.send_message(
         QueueUrl=queue_url,
